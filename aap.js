1bf5802559f7244d30cef3b61c9962d9de39a42a
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2024-10-25 08:55:32 -0300

    25/10

--- aap.js
+++ aap.js
@@ -1,3 +1,18 @@
 function criaCartao(categoria, pergunta, resposta){
-    console.log(categoria, pergunta, resposta);
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.classname = 'cartao';
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>${pergunta}5</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O JavaScript é uma linguagem de programação</p>
+                    </div>
+                </div>
+    `
+    container.appendChild(cartao);
 }
