a1803f96687fcd8733138f2256216b32b584e213
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2024-10-25 08:39:54 -0300

    25/10

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,27 +1,27 @@
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Respostas'
+    'conhecimentos gerais',
+    'De onde é a invenção do chuveiro elétrico?',
+    'brasil'
 
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Respostas'
+    'conhecimentos gerais',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e russia'
 
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Respostas'
+    'conhecimentos gerais',
+    'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom quixote'
 
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Respostas'
+    'conhecimentos gerais',
+    'Qual a nacionalidade de Che Guevara?',
+    'Argentina'
 
 )
