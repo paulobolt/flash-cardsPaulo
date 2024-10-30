4734c7f60823b2efd87c2b2eefb6b35e2ea0ede4
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2024-10-30 09:18:58 -0300

    30/10

--- aap.js
+++ aap.js
@@ -7,7 +7,7 @@ function criaCartao(categoria, pergunta, resposta){
     <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
-                        <p>${pergunta}5</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
                         <p>O JavaScript é uma linguagem de programação</p>
