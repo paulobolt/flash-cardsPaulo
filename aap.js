3c7def941c3ab57f080ade1ca06be52a9cdb12ba
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2024-11-22 11:16:34 -0300

    22/11

--- aap.js
+++ aap.js
@@ -14,5 +14,14 @@ function criaCartao(categoria, pergunta, resposta) {
                     </div>
                 </div>
     `
+
+    let respostaEstaVisivel = false;
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', viraCartao);
+    }
+    cartao.addEventListener('click', viraCartao);
+    
     container.appendChild(cartao);
 }
